72c3214e98bc9da59f2893d8a886f449d20c7f88
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-08-22 14:43:21 +0100

    feat: create `results` view function

--- polls/views.py
+++ polls/views.py
@@ -8,3 +8,7 @@ def index(request):
 
 def detail(request, question_id):
     return HttpResponse('You are looking at question %s.' % question_id)
+
+def results(request, question_id):
+    response = 'You are looking at the results of question %s.'
+    return HttpResponse(response % question_id)
