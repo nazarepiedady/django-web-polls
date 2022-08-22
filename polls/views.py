488f586187333cfc7c1b27742b3c2c37faed60ac
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-08-22 14:42:01 +0100

    feat: create `detail` view function

--- polls/views.py
+++ polls/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('Hello, World. You are at the polls index.')
+
+def detail(request, question_id):
+    return HttpResponse('You are looking at question %s.' % question_id)
