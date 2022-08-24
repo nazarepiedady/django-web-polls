b5c678369955409b634e381b7cb9a220679bac95
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-08-24 17:16:45 +0100

    chore: use a generic view for the `results` view

--- polls/urls.py
+++ polls/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     # example: /polls/
     path('', views.IndexView.as_view(), name='index'),
     # example: /polls/5/
-    path('<int:question_id>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     # example: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # example: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote')
 ]
