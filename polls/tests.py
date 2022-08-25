cc34641cb4bfb3bcf86cbc1ccbf5e6cc5511a500
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-08-25 19:48:15 +0100

    test: detail view should returns `not found` for future question

--- polls/tests.py
+++ polls/tests.py
@@ -110,4 +110,14 @@ class QuestionIndexViewTests(TestCase):
 
 
 class QuestionDetailViewTests(TestCase):
-    pass
+
+    def test_future_question(self):
+        '''
+        The detail view of a question with a pub_date in the future
+        returns a 404 not found.
+        '''
+        future_question = create_question(
+            question_text='Future question.', days=5)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
