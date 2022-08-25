aa0e342d95785bea88595a5c400e61642e4fdd6c
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-08-25 19:12:46 +0100

    test: polls index page should show multiple questions if there are

--- polls/tests.py
+++ polls/tests.py
@@ -93,3 +93,17 @@ class QuestionIndexViewTests(TestCase):
             response.context['latest_question_list'],
             [question],
         )
+
+    def test_two_past_questions(self):
+        '''
+        The questions index page may display multiple questions.
+        '''
+        first_question = create_question(
+            question_text='Past question 1.', days=-30)
+        second_question = create_question(
+            question_text='Past question 2.', days=-5)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            [second_question, first_question]
+        )
