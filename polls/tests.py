228108aaa2cf85dbf5b58f3ffa1f26ccdf9d22d3
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-08-25 18:46:16 +0100

    test: polls index view if no questions should contains empty `latest_question_list`

--- polls/tests.py
+++ polls/tests.py
@@ -50,4 +50,12 @@ class QuestionModelTests(TestCase):
 
 
 class QuestionIndexViewTests(TestCase):
-    pass
+
+    def test_no_questions(self):
+        '''
+        If no questions exist, an appropriate message is displayed.
+        '''
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'No polls are available.')
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
