b84acb8774babfa7905e76790a35263344ff83b9
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-08-30 20:34:18 +0100

    chore: create the structure of the `ChoiceInline` class

--- polls/admin.py
+++ polls/admin.py
@@ -3,7 +3,11 @@ from django.contrib import admin
 from .models import Choice, Question
 
 
-# Register your models here.
+# Customize your administration model here.
+class ChoiceInline(admin.modelAdmin):
+    pass
+
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['question_text']}),
@@ -11,4 +15,5 @@ class QuestionAdmin(admin.ModelAdmin):
     ]
 
 
+# Register your models here.
 admin.site.register(Question, QuestionAdmin)
